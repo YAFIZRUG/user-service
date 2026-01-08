6662bac70edfd610b53c35448fc6e70bbcc75db9
yafizrug <sanko.veselkin@bk.ru>
2026-01-09 00:43:02 +0300

    feat: add required microservice's patterns

--- src/main/java/edu/aston/Main.java
+++ src/main/java/edu/aston/Main.java
@@ -2,10 +2,12 @@ package edu.aston;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class Main {
 
     public static void main(String[] args) {
